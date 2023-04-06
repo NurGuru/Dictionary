9fbf7f53543ce7b2699ab96f8037b453d6b974f0
NurGuru <nurchibald@mail.ru>
2023-04-06 22:50:10 +0300

    DB-step3 добавил реализацию меню для статистики

--- src/main/kotlin/fiels.kt
+++ src/main/kotlin/fiels.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.roundToInt
 
 fun main() {
     val wordsFile = File("words.txt")
@@ -7,11 +8,26 @@ fun main() {
 
     for (line in lines) {
         val line = line.split("|")
-        val word = Word(line[0], line[1], line[2].toIntOrNull()?:0,)
+        val word = Word(line[0], line[1], line[2].toIntOrNull() ?: 0)
         dictionary.add(word)
     }
-    println(dictionary)
+    val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }.size
+    while (true) {
+        println("Меню: 1 – Учить слова, 2 – Статистика, 0 – Выход")
+        when (readln().toInt()) {
+            1 -> println("Вы выбрали учить слова")
+            2 -> println(
+                "Всего слов в словаре ${dictionary.size}\n" +
+                        "Выучено $learnedWords слов из ${dictionary.size}| " +
+                        "${((learnedWords.toDouble() / dictionary.size) * 100).roundToInt()}%"
+            )
+
+            0 -> break
+            else -> println("Такой команды нет, выберите нужную команду.")
+        }
+    }
 }
+
 data class Word(
     val original: String,
     val translate: String,
